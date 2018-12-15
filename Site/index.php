f4f037a8af451da2d703ba35ace4da930d354b06
Josh Hauser <33412940+npjoshh@users.noreply.github.com>
2018-12-15 16:36:53 +0100

    Connexion + déconnexion
    Connexion à partir d'une base de données pour faciliter les modifications d'identifiants si l'utilisateur le souhaite.

---
+++ Site/index.php
@@ -0,0 +1,35 @@
+<html lang="fr">
+<head>
+	<title>Gestion de clients</title>
+	<meta charset="UTF-8"/>
+	<link rel="stylesheet" href="home_style.css"/>
+</head>
+<body>
+	<h1>Connexion</h1>
+	<fieldset>
+	<form action='login.php' method='POST'>
+		<label>Nom d'utilisateur:</label><input type="text" name="login" required/>
+		<br/>
+		<label>Mot de passe:</label><input type="password" name="password" required/>
+		<br/>
+		<input type="submit" value="Connexion" />
+	</form>
+	</fieldset>
+
+<?php
+session_start();
+if(isset($_SESSION["user"]) and !empty($_SESSION["user"]))
+{
+	header("Location: intranet.php");
+}
+else
+{
+	session_destroy();
+}
+if(isset($_GET['id']))
+{
+	echo "<p> Le nom d'utilisateur ou le mot de passe n'existe pas dans notre base de données.</p>";
+}
+?>
+</body>
+</html>
