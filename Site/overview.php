2854d1f21b840ba74f3f7fbc23af7950fed714b2
Berzok <34751752+Berzok@users.noreply.github.com>
2018-12-17 15:50:40 +0100

    Add files via upload

--- Site/overview.php
+++ Site/overview.php
@@ -22,13 +22,11 @@
     $search_query = "Plante verte";
     $search_query = urlencode( $search_query );
     $html = file_get_html("https://www.google.com/search?client=firefox-b-ab&biw=1920&bih=903&tbm=isch&sa=1&ei=X3oXXI-TB4Scaai0urAI&q=plante+verte&oq=plante+verte&gs_l=img.3.0.35i39l2j0l5j0i30l3.1100.1256..1587...0.0..0.124.237.0j2......1....1..gws-wiz-img.EpH2qOC2SMk");
-	$image_count = 100; //Enter the amount of images to be shown
 	$tableau = Array();
 	$i = 0;
-	for($i=0; $i<100; $i++){
-		array_push($tableau, $html->find('img', $i));
-		echo $html->find('img', $i);
-		$i++;
+	foreach($html->find('img') as $element){
+		array_push($tableau, $element);
+		//echo $element. '<br>';
 	}
-	//echo $tableau;
+	echo $tableau[mt_rand(0, sizeof($tableau))];
 ?>
