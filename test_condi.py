77db6609c135038847e2732165fd70c9b5982e74
Kodlack <34751983+Kodlack@users.noreply.github.com>
2018-12-20 09:17:41 +0100

    Add files via upload
    Deux classes, une lisant les fichiers et les stockant dans des dict, une classe de test regroupant les tests et vérifiant si arrosage ou non

--- test_condi.py
+++ test_condi.py
@@ -1,53 +1,72 @@
 #coding: utf-8
 import json
 
-"""dict_data représente le dictionnaire de données lus dans le fichier Json, écrit par les capteurs
-val données correcpond à la valeur donnée par l'utilisateur, valeur conditionnelle de l'arrosage """
 
-
-def test_humidite(dict_data, val_donnee):
-
-	if (dict_data>=val_donnee):
-		return True
+class DonneesPlantesTest:
+    """Classe définissant les paramètres de test fournis par l'utilisateur 
+	ainsi que les données courantes des plantes analysées par les capteurs"""
+    
+    def __init__(self):
+        """dict_data représente le dictionnaire de données lus dans le fichier Json,
+		écrit par les capteurs, dict_test correcpond aux la valeurs donnée par 
+		l'utilisateur, valeur conditionnelle de l'arrosage """
 		
-	else:
-		return False
-
-def test_temperature(dict_data, val_donnee):
-
-	if (dict_data>=val_donnee):
-		return True
-		
-	else:
-		return False
-
-def test_debordement(dict_data):
-
-	if (dict_data>=val_donnee):
-		return True
-		
-	else:
-		return False
-		
-def test_luminosite(dict_data, val_donnee):
-
-	if (dict_data>val_donnee):
-		return True
-		
-	else:
-		return False
-
-def test_heure(val_donnee):
+        self.dict_data = lecture_data('data_capteurs.json')
+		self.dict_test = lecture_conditions('data_conditions.json')
 	
-	maintenant = datetime.now()
-	#on fixe la date courante avec l'heure actuelle
-	if (val_donnee>=maintenant.hour):
-		return True
-		
-	else:
-		return False
+	def lecture_fichier(nom_fichier):
+		with open(nom_fichier) as json_data:
+			dictionnaire = json.load(json_data)
+		return dictionnaire
 
-def arrosage():
+class TestsArrosage:
+ """Classe définissant les tests faisables par le programme.
+ Permet de savoir si les conditions d'arrosage son remplies ou non
+ et le cas échéant, déclencher un arrosage"""
+ 
+	def test_humidite(dict_data, val_donnee):
+
+		if (dict_data>=val_donnee):
+			return True
+		
+		else:
+			return False
+
+	def test_temperature(dict_data, val_donnee):
+
+		if (dict_data>=val_donnee):
+			return True
+		
+		else:
+			return False
+
+	def test_debordement(dict_data):
+
+		if (dict_data>=val_donnee):
+			return True
+		
+		else:
+			return False
+		
+	def test_luminosite(dict_data, val_donnee):
+
+		if (dict_data>val_donnee):
+			return True
+		
+		else:
+			return False
+
+	def test_heure(val_donnee):
+	
+		maintenant = datetime.now()
+		#on fixe la date courante avec l'heure actuelle
+		if (val_donnee>=maintenant.hour):
+			return True
+		
+		else:
+			return False
+
+	def arrosage():
 	#déclenche un arrosage immédiatement
 	
 	#Alimente la valve pour l'ouvrir et la fermer
@@ -58,54 +77,42 @@ def arrosage():
 	#on peut supposer que l'user souhaite que l'humidite soit dans un intervalle
 	#alors on fixe la limite d'arrosage a 5% pres de la borne superieur de l'intervalle
 
-
-def lecture_data():
-	with open('data_capteurs.json') as json_data:
-		data_dict = json.load(json_data)
-    return data_dict
-	
-def lecture_conditions():
-	with open('data_conditions.json') as json_data:
-		data_dict = json.load(json_data)
-    return data_dict
-
-def ecriture():
+	def ecriture():
 	
 
-def tests_choisis(dict_test, dict_data):
+	def tests_choisis(dict_test, dict_data):
+	"""Identifie les tests choisis par l'user et regarde si les conditions sont remplis pour l'arrosage"""
+		tab_test=[2]
+		#Ce tableau de booleens sert a garder les retours de fonctions de test en memoire
 	
-	tab_test=[2]
-	#Ce tableau de booleens sert a garder les retours de fonctions de test en memoire
+		if(dict_test[] == !None ):
+			#l'utilisateur a choisis d'arroser à une certaine heure
+			tab_test [0] = test_heure(dict_data[])
+			#On verifie si la condition d'heure est validee ou non
+			print "je test l'heure"
 	
-	if(dict_test[]==True):
-		#l'utilisateur a choisis d'arroser à une certaine heure
-		tab_test [0] = test_heure(dict_data[])
-		#On verifie si la condition d'heure est validee ou non
-		print "je test l'heure"
-	
-	if(dict_test[]==True):
-		#l'utilisateur a choisis d'arroser en fonction de l'humidité de la terre
-		tab_test [1] = test_humidite(dict_data[])
-		#On verifie si la condition d'humidite est valide ou non
-		print "je test l'humidite"
+		if(dict_test[] == !None):
+			#l'utilisateur a choisis d'arroser en fonction de l'humidité de la terre
+			tab_test [1] = test_humidite(dict_data[])
+			#On verifie si la condition d'humidite est valide ou non
+			print "je test l'humidite"
 		
-	if(dict_test[]==True):
-		#l'utilisateur a choisis d'arroser en fonction de la température ambiante
-		tab_test [2] = test_temperature([dict_data])
-		#On verifie si la condition de temperature est validee ou non
-		print "je test la temperature"
+		if(dict_test[] == !None):
+			#l'utilisateur a choisis d'arroser en fonction de la température ambiante
+			tab_test [2] = test_temperature([dict_data])
+			#On verifie si la condition de temperature est validee ou non
+			print "je test la temperature"
 	
-	for(i in tab_test):
-	#On parcours le tableau de booleens, si il y a au moins un False, on arrose pas,
-	#sinon on arrose
-		if (i == False)
-			return False
-	arrosage()
-	print"maintenant nous allons arroser"
+		for(i in tab_test):
+		#On parcours le tableau de booleens, si il y a au moins un False, on arrose pas,
+		#sinon on arrose
+			if (i == False)
+				return False
+		arrosage()
+		print"maintenant nous allons arroser"
 			
 print "Voici les tests"
-dict_data = lecture_data()
-dict_test = lecture_conditions()
+premieres_data = DonneesPlantesTest()
 #On stocke les donnees des fichiers JSON dans deux dictionnaires differents
 
 tests_choisis(dict_test, dict_data)
