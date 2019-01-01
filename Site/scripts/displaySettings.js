9d6df2644497732f1fc01b5e6c72261da41bd4b1
Josh Hauser <33412940+npjoshh@users.noreply.github.com>
2019-01-01 16:50:36 +0100

    Add files via upload

--- Site/scripts/displaySettings.js
+++ Site/scripts/displaySettings.js
@@ -7,27 +7,20 @@ function initSettings()
     aiActif.onchange = settings;
     aiInactif = document.getElementById("aiInactif");
     aiInactif.onchange = settings;
+
+    acActif = document.getElementById("acActif");
+    acActif.onchange = settings;
+    acInactif = document.getElementById("acInactif");
+    acInactif.onchange = settings;
 }
 
 function settings()
 {
-    if(aiInactif.checked)
-    {
-        document.getElementById("arroser").disabled = 1;
-        document.getElementById("ai").disabled = 0;
-        document.getElementById("ac").disabled = 0;
-        document.getElementById("frequence_tests").disabled = 0;
-        document.getElementById("uft").disabled = 0;
-        document.getElementById("frequence_arrosage").disabled = 0;
-        document.getElementById("ufa").disabled = 0;
-        document.getElementById("humidite").disabled = 0;
-        document.getElementById("valider").disabled = 0;
-    }
-    else
+    if(aiActif.checked)
     {
         document.getElementById("arroser").disabled = 0;
-        document.getElementById("ai").disabled = 1;
-        document.getElementById("ac").disabled = 1;
+        document.getElementById("acActif").disabled = 1;
+        document.getElementById("acInactif").disabled = 1;
         document.getElementById("frequence_tests").disabled = 1;
         document.getElementById("uft").disabled = 1;
         document.getElementById("frequence_arrosage").disabled = 1;
@@ -35,6 +28,42 @@ function settings()
         document.getElementById("humidite").disabled = 1;
         document.getElementById("valider").disabled = 1;
 
-
     }
+    else if(aiInactif.checked)
+    {
+        document.getElementById("arroser").disabled = 1;
+        document.getElementById("acActif").disabled = 0;
+        document.getElementById("acInactif").disabled = 0;
+        document.getElementById("frequence_tests").disabled = 0;
+        document.getElementById("uft").disabled = 0;
+        document.getElementById("frequence_arrosage").disabled = 0;
+        document.getElementById("ufa").disabled = 0;
+        document.getElementById("humidite").disabled = 0;
+        document.getElementById("valider").disabled = 0;
+    }
+
+    if(acActif.checked)
+    {
+        console.log(".");
+        document.getElementById("aiActif").disabled = 1;
+        document.getElementById("aiInactif").disabled = 1;
+        document.getElementById("arroser").disabled = 1;
+        document.getElementById("frequence_tests").disabled = 0;
+        document.getElementById("uft").disabled = 0;
+        document.getElementById("frequence_arrosage").disabled = 0;
+        document.getElementById("ufa").disabled = 0;
+        document.getElementById("humidite").disabled = 0;
+    }
+    else if(acInactif.checked && !aiInactif.checked)
+    {
+        document.getElementById("aiActif").disabled = 0;
+        document.getElementById("aiInactif").disabled = 0;
+        document.getElementById("arroser").disabled = 0;
+        document.getElementById("frequence_tests").disabled = 1;
+        document.getElementById("uft").disabled = 1;
+        document.getElementById("frequence_arrosage").disabled = 1;
+        document.getElementById("ufa").disabled = 1;
+        document.getElementById("humidite").disabled = 1;
+    }
+
 }
--- Site/system_settings.php
+++ Site/system_settings.php
@@ -29,40 +29,41 @@
 			<p>Temps d'arrosage: </p><p id="temps_arrosage"></p>
 			<input id="arroser" onclick="startSpraying();" type="button" name="lancement_arrosage" value="Arroser">
 		</div>
-		<fieldset>
 			<form action='' method='POST'>
-				<div id="arrosage_conditionnel">
-					<h2>Mode d'arrosage</h2>
-					<input id="ac" type="radio" value="Instantané" name="type_arrosage" required/><label>Instantané</label>
-					<br/>
-					<input id="ai" type="radio" value="Conditionnel" name="type_arrosage" required/><label>Conditionnel</label>
-					<br/>
-				</div>
+				<fieldset>
+					<div id="arrosage_conditionnel">
+						<h2>Arrosage conditionnel</h2>
+						<input id="acActif" type="radio" value="Actif" name="arrosage_conditionnel" required/><label>Actif</label>
+						<br/>
+						<input id="acInactif" type="radio" value="Inactif" name="arrosage_conditionnel" required/><label>Inactif</label>
+						<br/>
+					</div>
+				</fieldset>
 
+				<fieldset>
+					<div id="pac"><!--pac = Paramètres Arrosage Conditionnel-->
+						<h3>Fréquence de tests:</h3>
+						<input type="text" name="frequence_tests" id="frequence_tests">
+						<select name="uft" id="uft"><!--uft = unité de fréquence de tests (c-à-d minutes ou heures)-->
+							<option>minutes</option>
+							<option>heures</option>
+						</select>
 
-				<div id="pac"><!--pac = Paramètres Arrosage Conditionnel-->
-					<h2>Arrosage conditionnel</h2>
-					<h3>Fréquence de tests:</h3>
-					<input type="text" name="frequence_tests" id="frequence_tests">
-					<select name="uft" id="uft"><!--uft = unité de fréquence de tests (c-à-d minutes ou heures)-->
-						<option>minutes</option>
-						<option>heures</option>
-					</select>
+						<h3>Conditions d'arrosage</h3>
+						<p><label>Taux d'humidité: </label><input type="text" name="humidite" id="humidite"> %</p>
 
-					<h3>Conditions d'arrosage</h3>
-					<p><label>Taux d'humidité: </label><input type="text" name="humidite" id="humidite"> %</p>
+						<h3>Fréquence d'arrosage:</h3>
+						<input type="text" name="frequence_arrosage" id="frequence_arrosage">
+						<select id="ufa" name="ufa"><!--ufa = unité de fréquence d'arrosage (c-à-d minutes ou heures)-->
+							<option>minutes</option>
+							<option>heures</option>
+						</select>
+					</div>
 
-					<h3>Fréquence d'arrosage:</h3>
-					<input type="text" name="frequence_arrosage" id="frequence_arrosage">
-					<select id="ufa" name="ufa"><!--ufa = unité de fréquence d'arrosage (c-à-d minutes ou heures)-->
-						<option>minutes</option>
-						<option>heures</option>
-					</select>
-				</div>
-
-				<input id="valider" type="submit" name="valider" value="Valider">
+					<input id="valider" type="submit" name="valider" value="Valider">
+				</fieldset>
 			</form>
-			</fieldset>
+
 	</fieldset>
 
 <?php
