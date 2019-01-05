796e32f189eb200b90e310ff8768a2b67723b9f8
mikee92 <45966886+mikee92@users.noreply.github.com>
2019-01-05 15:27:06 +0100

    Add files via upload

--- Site/header.php
+++ Site/header.php
@@ -1,7 +1,6 @@
 <?php
 if(isset($_SESSION['login']))
 {
-
 }
 else
 {
@@ -10,17 +9,83 @@ else
 }
 ?>
 
-<div>
-    <a id="home" href=""><h1 id="titre">Arroz'oir</h1></a>
+
+<head>
+<html lang="fr">
+<html>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="css/css_header.css"/>
+
+</head>
+<div id = "container">
+	
+	<div id="headerarea">
+	
+		<div id="logo">
+		<a href="/Arroster-web/Site/header.php">
+			<img src = "img/logo.png" >
+		</a>
+		</div>
+    
+		<div id="search">
+			<form class = "s" method="post" name="headerseach">
+				<input id="searchtext" type="text" placeholder="rechercher..." >
+				<input id="boutonsearch" type="image" src="img/bouton.png" onClick="clickit(this);">
+					
+			</form>
+		</div>
+	
+	
+		
 		<nav id="menu">
 			<ul id="menu-links">
 				<li><a title="Accueil" href="overview.php">Accueil</a></li>
-                <li>&#124;</li>
-				<li><a title="États des plantes" href="states.php">États des plantes</a></p></li>
+				<li>&#124;</li>
+				<li><a title="États des plantes" href="overview.php">États des plantes</a></p></li>
 				<li>&#124;</li>
 				<li><a title="Paramètres du système" href="system_settings.php">Paramètres du système</a></li>
-                <li>&#124;</li>
-                <a href='logout.php'>Déconnexion</a>
+				<li>&#124;</li>
+				<li><a href='logout.php'>Déconnexion</a></li>
 			</ul>
 		</nav>
+		
+	</div>
 </div>
+
+
+
+
+<body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<footer>
+
+
+
+<h2>&copy;</h2>
+<p> Arroz'oir</p>
+
+
+
+</footer>
+
+
+
+
+
+
+</body>
+</html>
--- Site/system_settings.php
+++ Site/system_settings.php
@@ -1,12 +1,16 @@
 <html lang="fr">
 <head>
 	<meta charset="UTF-8"/>
-	<link rel="stylesheet" href="home_style.css"/>
+	<link rel="stylesheet" href="css/css_system.css"/>
 	<title>Arroz'oir - Paramètres du système</title>
+	<link rel="icon" type="image/png" href="img/_icon.png"/>
 	<script type="text/javascript" src="scripts/arrosageInstantane.js"></script>
 	<script type="text/javascript" src="scripts/displaySettings.js"></script>
 </head>
+
+
 <body>
+
 	<?php
 		session_start();
 		if(!isset($_SESSION['login']))
@@ -18,54 +22,58 @@
 			require_once('header.php');
 		}
 	?>
-
-	<fieldset>
-		<div id="arrosage_instantane">
-			<h2>Arrosage instantané</h2>
-			<!--ai = arroage instantané-->
-			<input id="aiActif" type="radio" value="Actif" name="arrosage_instantane" required/><label>Actif</label>
-			<br>
-			<input id="aiInactif" type="radio" name="arrosage_instantane" value="Inactif"><label>Inactif</label>
-			<p>Temps d'arrosage: </p><p id="temps_arrosage"></p>
-			<input id="arroser" onclick="startSpraying();" type="button" name="lancement_arrosage" value="Arroser">
-		</div>
+<div id="containerglobal">
+	<fieldset id="glob">
+		<img id="planteG" src="img/plante_settings.png" >
+		<img id="planteD" src="img/plante_settings.png" >
+		<fieldset id="instant">
+			<div id="arrosage_instantane">
+				<h2>Arrosage instantané</h2>
+				<!--ai = arrosage instantané-->
+				<input id="aiActif" type="radio" value="Actif" name="arrosage_instantane" required/><label>Actif</label>
+				<br>
+				<input id="aiInactif" type="radio" name="arrosage_instantane" value="Inactif"><label>Inactif</label>
+				<p>Temps d'arrosage: </p><p id="temps_arrosage"></p>
+				<input id="arroser" onclick="startSpraying();" type="button" name="lancement_arrosage" value="Arroser">
+			</div>
+		</fieldset>
+		
+		<fieldset id="cond">
 			<form action='' method='POST'>
-				<fieldset>
-					<div id="arrosage_conditionnel">
-						<h2>Arrosage conditionnel</h2>
-						<input id="acActif" type="radio" value="Actif" name="arrosage_conditionnel" required/><label>Actif</label>
-						<br/>
-						<input id="acInactif" type="radio" value="Inactif" name="arrosage_conditionnel" required/><label>Inactif</label>
-						<br/>
-					</div>
-				</fieldset>
+				<div id="arrosage_conditionnel">
+					<h2>Mode d'arrosage</h2>
+					<input id="ac" type="radio" value="Instantané" name="type_arrosage" required/><label>Instantané</label>
+					<br/></br>
+					<input id="ai" type="radio" value="Conditionnel" name="type_arrosage" required/><label>Conditionnel</label>
+					<br/>
+				</div>
 
-				<fieldset>
-					<div id="pac"><!--pac = Paramètres Arrosage Conditionnel-->
-						<h3>Fréquence de tests:</h3>
-						<input type="text" name="frequence_tests" id="frequence_tests">
-						<select name="uft" id="uft"><!--uft = unité de fréquence de tests (c-à-d minutes ou heures)-->
-							<option>minutes</option>
-							<option>heures</option>
-						</select>
 
-						<h3>Conditions d'arrosage</h3>
-						<p><label>Taux d'humidité: </label><input type="text" name="humidite" id="humidite"> %</p>
+				<div id="pac"><!--pac = Paramètres Arrosage Conditionnel-->
+					<h2>Arrosage conditionnel</h2>
+					<h3>Fréquence de tests:</h3>
+					<input type="text" name="frequence_tests" id="frequence_tests">
+					<select name="uft" id="uft"><!--uft = unité de fréquence de tests (c-à-d minutes ou heures)-->
+						<option>minutes</option>
+						<option>heures</option>
+					</select>
 
-						<h3>Fréquence d'arrosage:</h3>
-						<input type="text" name="frequence_arrosage" id="frequence_arrosage">
-						<select id="ufa" name="ufa"><!--ufa = unité de fréquence d'arrosage (c-à-d minutes ou heures)-->
-							<option>minutes</option>
-							<option>heures</option>
-						</select>
-					</div>
+					<h3>Conditions d'arrosage</h3>
+					<p><label>Taux d'humidité: </label><input type="text" name="humidite" id="humidite"> %</p>
 
-					<input id="valider" type="submit" name="valider" value="Valider">
-				</fieldset>
+					<h3>Fréquence d'arrosage:</h3>
+					<input type="text" name="frequence_arrosage" id="frequence_arrosage">
+					<select id="ufa" name="ufa"><!--ufa = unité de fréquence d'arrosage (c-à-d minutes ou heures)-->
+						<option>minutes</option>
+						<option>heures</option>
+					</select>
+				</div>
+
+				<input id="valider" type="submit" name="valider" value="Valider">
 			</form>
-
+		</fieldset>
 	</fieldset>
-
+</div>
 <?php
 
 ?>
